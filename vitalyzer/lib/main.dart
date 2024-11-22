ec6f466ed9fab50fb3a88ca080513d071e340640
veysel-ozdemir <wishell.rock.4@gmail.com>
2024-11-22 20:33:02 +0300

    Chore: Disable screen rotation in app

--- vitalyzer/lib/main.dart
+++ vitalyzer/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vitalyzer/app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const VitalyzerApp());
+  // Lock the app orientation to portrait mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const VitalyzerApp());
+  });
 }
